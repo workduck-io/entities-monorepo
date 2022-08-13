89a37793cb618c7d7039c76b402597e3394bd2d6
rpPanda <ruddhi@workduck.io>
2022-08-14 00:31:39 +0530

    Moved utilities to separate library

--- libs/entity-utils/src/lib/utils.ts
+++ libs/entity-utils/src/lib/utils.ts
@@ -1,5 +1,3 @@
-import { MAX_DYNAMO_BATCH_REQUEST } from './consts';
-
 export const getEndpoint = () => {
   if (process.env.AWS_EXECUTION_ENV) {
     return undefined;
@@ -20,37 +18,6 @@ export const getRegion = () => {
   } else {
     return 'local';
   }
-};
-
-export const chunkify = <T>(
-  array: T[],
-  chunkSize = MAX_DYNAMO_BATCH_REQUEST
-) => {
-  const chunkifiedArr: T[][] = [];
-  for (let i = 0; i < array.length; i += chunkSize) {
-    const chunk = array.slice(i, i + chunkSize);
-    chunkifiedArr.push(chunk);
-  }
-  return chunkifiedArr;
-};
-
-type Promisable<T> = T | Promise<T>;
-
-type Iterator<T, U> = (item: T) => Promisable<U>;
-
-export const batchPromises = async <T, U>(
-  batchSize: number,
-  collection: Promisable<T[]>,
-  callback: Iterator<T, U>
-): Promise<U[]> => {
-  const arr = await Promise.resolve(collection);
-  return arr
-    .map((_, i) => (i % batchSize ? [] : arr.slice(i, i + batchSize)))
-    .map(
-      (group) => (res) =>
-        Promise.allSettled(group.map(callback)).then((r) => res.concat(r))
-    )
-    .reduce((chain, work) => chain.then(work), Promise.resolve([]));
 };
 
 export default () => process.env.DATA_STORE_ARN.split('/').slice(-1)[0];
