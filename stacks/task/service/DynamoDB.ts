0877e3e1f8b82574a738efb7fca5eb8b2a4b1ffb
rpPanda <ruddhi@workduck.io>
2022-10-08 16:14:03 +0530

    Hotfix

--- stacks/task/service/DynamoDB.ts
+++ stacks/task/service/DynamoDB.ts
@@ -2,4 +2,10 @@ import { initializeTable } from '@mex/entity-utils';
 
 export const taskTable = initializeTable({
   name: `${process.env.SLS_STAGE}-task-store`,
+  additionalIndexes: {
+    'ak-pk-index': {
+      partitionKey: 'ak',
+      sortKey: 'pk',
+    },
+  },
 });
