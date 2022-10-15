b41400d27753a5ce11226bbe1daa9b703710033d
rpPanda <ruddhi@workduck.io>
2022-10-16 01:30:26 +0530

    Routing Library Added

--- libs/gen-utils/src/lib/interfaces.ts
+++ libs/gen-utils/src/lib/interfaces.ts
@@ -1,39 +1,3 @@
-import type {
-  APIGatewayProxyEventV2,
-  APIGatewayProxyResultV2,
-  Handler,
-} from 'aws-lambda';
-
-export enum HTTPMethod {
-  'POST' = 'POST',
-  'GET' = 'GET',
-  'PUT' = 'PUT',
-  'DELETE' = 'DELETE',
-  'PATCH' = 'PATCH',
-}
-
-export type EventHandlerMap = {
-  handler: ValidatedAPIGatewayProxyHandler<any>;
-  method: HTTPMethod;
-  path: string;
-};
-
-export type EventHandlerPairs = {
-  [routeKey: string]: ValidatedAPIGatewayProxyHandler<any>;
-};
-
-export type ValidatedAPIGatewayProxyEvent<S> = Omit<
-  APIGatewayProxyEventV2,
-  'body'
-> & {
-  body?: S;
-};
-
-export type ValidatedAPIGatewayProxyHandler<S> = Handler<
-  ValidatedAPIGatewayProxyEvent<S>,
-  APIGatewayProxyResultV2
->;
-
 export interface WDTokenDecode {
   at_hash: string;
   sub: string;
